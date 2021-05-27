591cf50261b47dcb9ef725d7daf6edc30d22823c
oldwisesage <gandalf@Sages-MacBook-Pro-2.local>
2021-05-26 20:04:01 -0400

    Scss configuration

--- comps/Nav.js
+++ comps/Nav.js
@@ -1,25 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "../styles/Nav.module.scss";
 
 const Nav = () => {
   return (
-    <nav>
-      <div className="logo">
-        <h1>meat friends</h1>
+    <nav className={styles.nav}>
+      <div className={styles.logo}>
+        <h1 className={styles.title}>meat friends</h1>
       </div>
-      <Link href="/">
-        <a>About</a>
-      </Link>
-      <Link href="/">
-        <a>Shop</a>
-      </Link>
-      <Link href="/">
-        <a>Philosophy</a>
-      </Link>
-      <Link href="/">
-        <a>Contact</a>
-      </Link>
-      <div className="shopping-cart">logo</div>
+      <div className={styles.links}>
+        <Link href="/">
+          <a className={styles.navlinks}>About</a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navlinks}>Shop</a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navlinks}>Philosophy</a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navlinks}>Contact</a>
+        </Link>
+      </div>
+      <div className={styles.cart}>
+        <Image src="/1f6d2.png" width={50} height={50} />
+      </div>
     </nav>
   );
 };
