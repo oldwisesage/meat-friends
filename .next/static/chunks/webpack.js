20ef6fa2c2bea51bc9861df16e6c4ebd1a1c1109
Sage Winn <sage.m.winn@gmail.com>
2021-06-11 14:40:34 -0400

    added css reset & normalize

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "abc77afc37ad5d1a3e4f"; }
+/******/ 		__webpack_require__.h = function() { return "559e6db579cbf64f1815"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
